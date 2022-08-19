75afd44672fd431fc4def842710cf3c88682a13e
Muhammad younas Qayyum <muhammadyounasqayyum@gmail.com>
2022-08-19 22:25:14 +0500

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const About = () => { 
+    return (
+        <div>
+            <div className="card mb-3 card text-center">
+                <h1 className="card-title">About</h1>
+                <img src="https://images.unsplash.com/photo-1517842645767-c639042777db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bm90ZXN8ZW58MHx8MHx8&w=1000&q=80" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    
+                    <h6 className="card-text">In this iNotebook user created their own account and store their own notes. In this app user update or delete their own notes. </h6>
+                    
+                </div>
+            </div>
+                
+                
+        </div>
+    )
+}
+
+export default About
